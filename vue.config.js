86ca45cff1d7586f9735f0807ecb7c36f71619c3
SevenTrap <749878662@qq.com>
2022-01-20 22:17:49 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   outputDir: "dist",
-  publicPath: process.env.NODE_ENV === "production" ? "/new-kg-web/" : "/",
+  publicPath: "/new-kg-web/",
   productionSourceMap: false,
   devServer: {
     port: 8086
